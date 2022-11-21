753895353dc905937513243c7fff3455b7babe39
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-11-21 15:39:59 -0500

    user route added

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
+
+// /api/users GET and POST
+router.route("/").get(getUser).post(createUser);
+
+// /api/users/:userID GET one user, PUT and DELETE by userID
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userID/friends/:friendId POST and DELETE by friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
